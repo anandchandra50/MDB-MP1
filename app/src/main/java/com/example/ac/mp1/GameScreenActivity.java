6de4993730e26a3256aff1faed3895fdcb70dd72
anandchandra50 <anandchandra50@gmail.com>
2018-09-07 16:58:30 -0700

    finished -- only further commits will be small ui etc.

--- app/src/main/java/com/example/ac/mp1/GameScreenActivity.java
+++ app/src/main/java/com/example/ac/mp1/GameScreenActivity.java
@@ -1,8 +1,10 @@
 package com.example.ac.mp1;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.CountDownTimer;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -46,6 +48,17 @@ public class GameScreenActivity extends AppCompatActivity {
 
         // init image
         image = findViewById(R.id.currentImage);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, correctName);
+                startActivity(intent);
+
+
+            }
+        });
 
         // init buttons
         opt1 = findViewById(R.id.option1);
